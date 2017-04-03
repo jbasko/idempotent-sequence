4d2ded6ccea705335614ee8caf035c9309e72b3a
Jazeps Basko <jazeps.basko@gmail.com>
2017-04-04 00:17:28 +0100

    Use command function docstring as command description in command listing

--- idemseq/examples/example01.py
+++ idemseq/examples/example01.py
@@ -10,11 +10,17 @@ example = SequenceBase()
 
 @example.command(run_always=True)
 def greeting(name):
+    """
+    Prints a greeting using name supplied in context. 
+    """
     print('Hello {}'.format(name))
 
 
 @example.command
 def step_one():
+    """
+    First things first. This docstring will appear as command description.
+    """
     pass
 
 
@@ -45,14 +51,17 @@ def main():
     parser.add_argument('--list', action='store_true', help='List all commands and exit')
     parser.add_argument('--command', help='Attempt to run a single command, will fail if previous commands have not been completed')
 
+    # Run options
     parser.add_argument('--dry-run', action='store_true', help='Just simulate a run')
     parser.add_argument('--start-at', help='Specify command to start with irrespective of completion of previous commands')
     parser.add_argument('--stop-before', help='Specify command before which to stop sequence execution')
+    parser.add_argument('--force', action='store_true')
+    parser.add_argument('--warn-only', action='store_true')
 
     args = parser.parse_args()
 
     context = dict(name=args.name)
-    sequence = example(args.run_id, dry_run=args.dry_run, start_at=args.start_at, stop_before=args.stop_before)
+    sequence = example(args.run_id, dry_run=args.dry_run, start_at=args.start_at, stop_before=args.stop_before, force=args.force, warn_only=args.warn_only)
 
     if args.reset:
         sequence.reset()
@@ -61,6 +70,8 @@ def main():
     if args.list:
         for command in sequence:
             print(' * {} - {}'.format(command.name, 'completed' if command.is_finished else 'todo'))
+            if command.description:
+                print(command.description)
         return
 
     with sequence.env(context=context):
--- tests/test_command.py
+++ tests/test_command.py
@@ -43,3 +43,36 @@ def test_equality():
 def test_str_representation():
     c1 = Command(func=lambda: 1, name='first')
     assert str(c1) == 'Command(name=first)'
+
+
+def test_func_doc_is_command_description():
+    def do_something():
+        """
+        This does something.
+        """
+
+    c1 = Command(func=do_something, name='c1')
+    assert c1.description == '\n        This does something.\n        '
+
+    c2 = Command(func=lambda: 1, name='c2')
+    assert c2.description is None
+
+    from idemseq.sequence import SequenceBase
+    sequence = SequenceBase(c1, c2)()
+    assert sequence['c1'].description == '\n        This does something.\n        '
+    assert sequence['c2'].description is None
+
+
+def test_func_doc_is_command_description_for_decorator_declared_commands():
+    from idemseq.sequence import SequenceBase
+    base = SequenceBase()
+
+    @base.command
+    def c3():
+        """This does not do much"""
+
+    assert base['c3'].description == 'This does not do much'
+
+    sequence = base()
+
+    assert sequence['c3'].description == 'This does not do much'
