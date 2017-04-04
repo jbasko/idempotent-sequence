df5219829e02e53e864653402172574cd4d27cf4
Jazeps Basko <jazeps.basko@gmail.com>
2017-04-04 23:36:46 +0100

    String representation of exceptions

--- idemseq/exceptions.py
+++ idemseq/exceptions.py
@@ -3,8 +3,17 @@ class SequenceException(Exception):
         self.sequence = sequence
         self.message = message
 
+    def __str__(self):
+        return '(message={}, sequence={})'.format(self.message, self.sequence)
+
+    def __repr__(self):
+        return '<{} {}>'.format(self.__class__.__name__, self)
+
 
 class SequenceCommandException(SequenceException):
     def __init__(self, sequence_command, message):
         super(SequenceCommandException, self).__init__(sequence_command._sequence, message)
         self.command = sequence_command
+
+    def __str__(self):
+        return '(message={}, command={})'.format(self.message, self.command)
